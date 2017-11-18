c14b0867132b73d81784e0c8e9c66ca89d343d06
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-11-18 15:26:43 +0900

    fix

--- GeofenceManageServer/config/config.go
+++ GeofenceManageServer/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+//DB Config
+const (
+
+	// DBMS add database management service
+	DBMS = "mysql"
+
+	// DBNAME add database name
+	DBNAME = ""
+
+	// DBUSER add database user
+	DBUSER = "root"
+
+	// DBPASS add database pass
+	DBPASS = ""
+
+	//DBPROTOCOL add database protocol
+	DBPROTOCOL = "tcp(192.168.33.10:3306)"
+
+	//DBOPTIONS add database options
+	DBOPTIONS = "charset=utf8&parseTime=true&loc=Local"
+
+	//DBDSN add DB source name
+	DBDSN = DBUSER + ":" + DBPASS + "@" + DBPROTOCOL + "/" + DBNAME + "?" + DBOPTIONS
+)
